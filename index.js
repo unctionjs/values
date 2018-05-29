08a098b087d2fa1bafe5340dc4b6d878cf6b2609
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 17:27:36 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import flip from "@unction/flip"
-import append from "@unction/append"
-import reduceValues from "@unction/reducevalues"
-
-export default function values (functor: FunctorType): Array<ValueType> {
-  return reduceValues(flip(append))([])(functor)
+import flip from "@unction/flip";
+import append from "@unction/append";
+import reduceValues from "@unction/reducevalues";
+export default function values(functor) {
+  return reduceValues(flip(append))([])(functor);
 }
