f74bfc277a59d17f18ebf8cb0616f6b369cde677
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:51 -0800

    Removing more bad types

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ![Stability][BADGE_STABILITY]
 ![Dependencies][BADGE_DEPENDENCY]
 
-> Array<A> | Set<A> | RecordType<unknown, A> | string => Array<A>
+> Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string => Array<A>
 
 Takes an iterable and returns it's values.
 
--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import flip from "@unction/flip";
 import append from "@unction/append";
 import reduceValues from "@unction/reducevalues";
 
-export default function values<A> (enumerable: Array<A> | Set<A> | RecordType<unknown, A> | string): Array<A> {
+export default function values<A> (enumerable: Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string): Array<A> {
   return reduceValues(flip(append))([])(enumerable);
 }
